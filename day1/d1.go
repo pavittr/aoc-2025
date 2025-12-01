85ba1999f1004d517965741fb942b76d4515c997
Ross Pavitt <rosspavitt@gmail.com>
2025-12-01 08:10:39 +0000

    Create d1.go

---
+++ day1/d1.go
@@ -0,0 +1,34 @@
+package day1
+
+type buff struct {
+  prev *buff
+  next *buff
+  value int
+}
+
+func solve() {
+
+  start := &buff {
+    prev: &buff{
+      value: 49,
+      },
+    next: &buff{
+      value:51,
+      },
+    value:50,
+    }
+  
+
+  current := start
+  prev := start.prev
+  next := start next
+  var next *buff
+  for int i := 0; i< 50 ;i++{
+    
+
+    }
+  
+
+
+
+}
